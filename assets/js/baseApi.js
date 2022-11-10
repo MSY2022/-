d67bf6d111d98c6f130c5727c4be9e55156858d5
MeiShiYu <970871216@qq.com>
2022-11-10 20:31:45 +0800

    完成了一些需求

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,9 @@
-$.ajaxPrefilter(function(options){
-    options.url = 'http://big-event-api-t.itheima.net'+options.url
+$.ajaxPrefilter(function (options) {
+
+    options.url = 'http://big-event-api-t.itheima.net' + options.url
+    if (options.url.includes('/my/') === true) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function () {
+    const layer = layui.layer;
+    getUserInfo()
+    document.querySelector('#tuichu').addEventListener('click', function () {
+        layer.confirm('确定退出?', { icon: 3, title: '提示' }, function (index) {
+            location.href = './login.html'
+            localStorage.removeItem('token')
+            layer.close(index)
+
+        })
+    })
+
+
+})
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+            return layer.msg("获取用户信息失败")
+            }
+           
+            renderAvatar(res.data)
+        },
+        complete: function (res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                location.href = './login.html'
+                localStorage.removeItem('token')
+                console.log(1);
+            }
+        }
+    })
+
+}
+function renderAvatar(user) {
+    const name = user.nickname || user.username
+    console.log(name);
+    $('.welcome').html(name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').prop('src', user.user_pic).show().siblings('.text_avatar').hide()
+    } else {
+        $('.text_avatar').html(name.substring(0, 1).toUpperCase()).show().siblings('.layui-nav-img').hide()
+    }
+}
